453f331a2d48577370fd481aa9fc1c29d5f51b6d
d4l4-33 <babtzorac@gmail.com>
2026-04-13 17:08:33 +0200

    setting up GitHub hosting

--- src/generate_page.py
+++ src/generate_page.py
@@ -8,7 +8,7 @@ def extract_title(markdown):
             return block.strip("# ")
     raise Exception("ERROR: No header found in markdown")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         markdown = f.read()
@@ -16,17 +16,19 @@ def generate_page(from_path, template_path, dest_path):
         template = f.read()
     template = template.replace("{{ Title }}", extract_title(markdown))
     template = template.replace("{{ Content }}", markdown_to_html_node(markdown).to_html())
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     with open(dest_path, "w") as f:
         f.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for item in os.listdir(dir_path_content):
         item_path = os.path.join(dir_path_content, item)
         dest_path = os.path.join(dest_dir_path, item)
         if os.path.isfile(item_path):
             dest_path = dest_path.replace(".md", ".html")
-            generate_page(item_path, template_path, dest_path)
+            generate_page(item_path, template_path, dest_path, basepath)
         else:
             if not os.path.isdir(dest_path):
                 os.mkdir(dest_path)
-            generate_pages_recursive(item_path, template_path, dest_path)
+            generate_pages_recursive(item_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 import os
 import shutil
+import sys
 from recursive_file_copy import recursive_file_copy
 from generate_page import generate_pages_recursive
 
-def main():
-    
-    if os.path.exists("./public"):
-        shutil.rmtree("./public")
 
-    recursive_file_copy("./static", "./public")
-    generate_pages_recursive("./content", "./template.html", "./public")
+def main():
+    basepath = sys.argv
+    
+    if os.path.exists("./docs"):
+        shutil.rmtree("./docs")
+
+    recursive_file_copy("./static", "./docs")
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
     
 if __name__ == "__main__":
     main()
