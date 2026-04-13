96ac3ffe185e8769e8b385fb3fec47a07a36c616
d4l4-33 <babtzorac@gmail.com>
2026-04-13 17:30:41 +0200

    bugfixing site host

--- src/main.py
+++ src/main.py
@@ -5,14 +5,17 @@ from recursive_file_copy import recursive_file_copy
 from generate_page import generate_pages_recursive
 
 
+
 def main():
-    basepath = sys.argv
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     
     if os.path.exists("./docs"):
         shutil.rmtree("./docs")
 
     recursive_file_copy("./static", "./docs")
-    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath[0])
     
 if __name__ == "__main__":
     main()
